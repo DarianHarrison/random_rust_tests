c05abf6a1f774df660457492d37627cdbf2b0b4c
Darian Harrison <darianharrison89@gmail.com>
2022-02-23 18:00:08 -0600

    randomness

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # random_rust_tests
  randomness testing
+
+```bash
+git clone https://github.com/DarianHarrison/random_rust_tests
+```
+```bash
+cd random_rust_tests
+cargo run
+```
--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,39 @@
 // this is how we benchmark our top k hitters
-    use rand::{thread_rng, Rng, distributions::{Distribution, Uniform}};
-
+use rand::{
+    Rng,
+    thread_rng, 
+    distributions::{
+        Distribution, 
+        Uniform
+    }, 
+    SeedableRng,
+    rngs::StdRng,
+};
 
 fn main() {
 
-    // fixed seed
-
-
-
+    // seeded pseudo random number generator
+    let mut rng = StdRng::seed_from_u64(10); // seeded
+    let message = Uniform::from(25..50);
+    let a = message.sample(&mut rng);
+    println!("{:?}", a); // should always be the same
 
     // random tuple
     let mut rng = thread_rng();
     let tuple: (u8, u8) = rng.gen();
+    println!("{:?}", tuple);
 
     // simulate a stream buffer of random values
-    let between = Uniform::from(25..50);
+    let message = Uniform::from(25..50);
     let mut rng = rand::thread_rng();
     let mut sum = 0;
-    for _ in 0..100 { // buffer of 100 random values
-        println!("{}", between.sample(&mut rng));
+    for _ in 0..10 { // buffer of 10 random values
+        println!("{}", message.sample(&mut rng));
     }
 
     // most efficient way of generating vectors of random values between range with uniform distribution
     let mut rng = rand::thread_rng();
-    let range = Uniform::new(0, 20);
+    let range = Uniform::from(0..20);
     let vals: Vec<u8> = (0..10).map(|_| rng.sample(&range)).collect();
-
-
+    println!("{:?}", vals);
 }
